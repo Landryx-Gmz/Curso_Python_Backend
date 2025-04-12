2db5ca509b12194d84bf9467bba0026b0fe8bbfb
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-04-12 12:23:38 +0200

    Nuevo_ejemplo_decoradores

--- Udemy/TemasAvanzados/decoradores.py
+++ Udemy/TemasAvanzados/decoradores.py
@@ -13,3 +13,23 @@ def saludar (nombre):
     print(f'Hola {nombre}')
 
 saludar('Andres')
+
+# Ejemplo:
+def palabras_descripcion(funcion):
+    def envoltorio_wrapper(*args, **kwargs):
+        print('Ejecutando Programa de calculo')
+        funcion(*args, *kwargs)
+        print('Gracias por usar nuestro programa')
+    return envoltorio_wrapper
+
+@palabras_descripcion
+def descript(operador):
+    print(f'Se utlizara {operador} para el calculo')
+
+@palabras_descripcion
+def suma(x,y):
+    calculo = x + y
+    print(f'La suma de {x} y de {y} es: {calculo}')
+
+descript("suma")
+suma(4,6)
