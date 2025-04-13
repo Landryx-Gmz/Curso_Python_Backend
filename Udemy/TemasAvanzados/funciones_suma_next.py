fee4e733f185bb535c870b58bc5338b3491f9b9f
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-04-13 11:54:49 +0200

    añadido_funciones_suma_next.py_la_explicacion

---
+++ Udemy/TemasAvanzados/funciones_suma_next.py
@@ -0,0 +1,17 @@
+# Función sum y next
+
+#****Funcion sum()******
+lista = [1,2,3,4,5]
+
+# Con la funcion sum() pasando por parametros el nombre de la lista sumamos todos los elementos.
+
+resultado = sum(lista)
+print(f'El rsultado de las suma de la lista es: {resultado}')
+
+# Podemos proporcionar un valor inicial
+resultado = sum(lista, 20)
+print(f"Resultado en la suma con valor inicial 20: {resultado}")
+
+
+
+
