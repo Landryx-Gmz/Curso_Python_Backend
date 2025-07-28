811869da46a0c513bfbe88a54c1220e9bdb85abf
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-07-28 18:09:17 +0200

    Refactorizacion_de_funciones_lambda_con_map

--- Udemy/2._TemasAvanzados/4._funciones_lambda.py
+++ Udemy/2._TemasAvanzados/4._funciones_lambda.py
@@ -3,6 +3,8 @@
 # Funcion cuadrado sin usar lambda
 def cuadrado(x):
     return x ** 2
+
+
 print(f'Funcion sin lambda para el cuadrado de 5 es: {cuadrado(5)}')
 
 # Funcion lambda(es anonima y  tiene return incorporado)
@@ -10,3 +12,12 @@ print(f'Funcion sin lambda para el cuadrado de 5 es: {cuadrado(5)}')
 cuadrado_lambda = lambda x: x ** 2
 print(f'La funcion lambda para el cuadrado de 2 es: {cuadrado_lambda(2)}')
 print(f'La funcion lambda para el cuadrado de 4 es: {cuadrado_lambda(4)}')
+
+# Con ma y lambda
+# Acreamos una lista de numeror
+numeros = [1, 2, 3, 4, 5]
+
+# Aplicar una funicion lamba para obtener el cuadrado de cada numero
+
+cuadrados = list(map(lambda x: x ** 2, numeros))
+print(f'Resultado de usar map y lambda: {cuadrados}')
